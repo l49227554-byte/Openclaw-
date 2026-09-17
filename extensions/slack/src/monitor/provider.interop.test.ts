58652667e2956d3fe0946f0d6c32f63620534ba9
Dallin Romney <dallinromney@gmail.com>
2026-09-16 23:22:23 -0700

    fix(test): narrow Slack socket payload bytes

--- extensions/slack/src/monitor/provider.interop.test.ts
+++ extensions/slack/src/monitor/provider.interop.test.ts
@@ -507,9 +507,9 @@ describe("createSlackBoltApp", () => {
         socket.on("message", (data) => {
           const bytes = Array.isArray(data)
             ? Buffer.concat(data)
-            : data instanceof ArrayBuffer
-              ? Buffer.from(data)
-              : data;
+            : Buffer.isBuffer(data)
+              ? data
+              : Buffer.from(data);
           acknowledgements.push(JSON.parse(bytes.toString("utf8")).envelope_id);
         });
         socket.send(JSON.stringify({ type: "hello" }));

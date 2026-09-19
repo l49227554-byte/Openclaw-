openclaw/openclaw

fix(memory-host-sdk): inherit execution.defaults.timeoutMs into resolved phases
resolveExecutionConfig only emitted timeoutMs when the phase-local record
contained one, so a config that set only execution.defaults.timeoutMs
(documented default path for #138403) produced resolved light/deep/rem
executions without the value — every narrative forward then received
undefined and fell back to the 60s hardcoded timeout.

Match the other optional fields (speed/thinking/budget/model all use
fallback inheritance): inherit fallback.timeoutMs, keep a valid phase-local
override authoritative. Regression test covers defaults -> all three phases
and a per-phase override; it fails on the pre-fix resolver.

src/memory-host-sdk/dreaming.ts
@@ -269,7 +269,7 @@ function resolveExecutionConfig(
 ): MemoryDreamingExecutionConfig {
   const record = asNullableRecord(value);
   const maxOutputTokens = normalizeOptionalPositiveInt(record?.maxOutputTokens);
-  const timeoutMs = normalizeOptionalPositiveInt(record?.timeoutMs);
+  const timeoutMs = normalizeOptionalPositiveInt(record?.timeoutMs) ?? fallback.timeoutMs;
   const temperatureRaw = record?.temperature;
   const temperature =
     typeof temperatureRaw === "number" && Number.isFinite(temperatureRaw) && temperatureRaw >= 0

l49227554-byte/Openclaw-

src/memory-host-sdk/dreaming.ts
@@ -355,7 +355,7 @@ function resolveExecutionConfig(
 ): MemoryDreamingExecutionConfig {
   const record = asNullableRecord(value);
   const maxOutputTokens = parseStrictPositiveInteger(record?.maxOutputTokens);
-  const timeoutMs = parseStrictPositiveInteger(record?.timeoutMs);
+  const timeoutMs = parseStrictPositiveInteger(record?.timeoutMs) ?? fallback.timeoutMs;
   const temperatureRaw = record?.temperature;
   const temperature =
     typeof temperatureRaw === "number" && Number.isFinite(temperatureRaw) && temperatureRaw >= 0